bf38ed77469d5b2a0969e127fd93cef3a646a783
Hemisphera <info@hemisphera.com>
2018-03-04 21:40:43 +0100

    Fix: WriteStringValueMessage skips first result character because it seems to be unrelated to the data itself

--- Hsp.Kemper.Driver/Messages/WriteStringValueMessage.cs
+++ Hsp.Kemper.Driver/Messages/WriteStringValueMessage.cs
@@ -12,7 +12,7 @@ namespace Hsp.Kemper.Driver
     {
       var page = dataBlock[8];
       var address = dataBlock[9];
-      var value = dataBlock.Skip(9).TakeWhile(c => c != 0).ToArray();
+      var value = dataBlock.Skip(10).TakeWhile(c => c != 0).ToArray();
       var str = Encoding.ASCII.GetString(value);
       return new WriteStringValueMessage(page, address, str);
     }
@@ -36,6 +36,10 @@ namespace Hsp.Kemper.Driver
       }.Concat(Encoding.ASCII.GetBytes(Value)).Concat(new byte [] { 0x00 }).ToArray();
     }
 
+    public override string ToString()
+    {
+      return Value;
+    }
   }
 
 }
